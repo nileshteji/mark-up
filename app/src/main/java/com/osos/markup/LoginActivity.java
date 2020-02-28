8a346cbb601532a5f6f163dfd13b7cf5a547513a
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-02-28 21:12:06 +0530

    Version 1.10

--- app/src/main/java/com/osos/markup/LoginActivity.java
+++ app/src/main/java/com/osos/markup/LoginActivity.java
@@ -3,6 +3,7 @@ package com.osos.markup;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,7 +35,7 @@ FirebaseAuth mAuth;
                  @Override
                  public void onComplete(@NonNull Task<AuthResult> task) {
                          if(task.isSuccessful()){
-                             startActivity();
+
                          }
                  }
              });
