ce1df19da7e3040dca0c51bb2f40cdb5d2034acd
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-03-28 12:52:48 +0530

    Version 2.2

--- app/src/main/java/com/osos/markup/Teacher.java
+++ app/src/main/java/com/osos/markup/Teacher.java
@@ -1,11 +1,18 @@
 package com.osos.markup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -17,20 +24,59 @@ DrawerLayout boj;
 ActionBarDrawerToggle drawerToggle;
 NavigationView navigationView;
 ImageView attendClass;
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
         attendClass=findViewById(R.id.android_class);
+        toolbar=findViewById(R.id.toolbar);
 
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+       setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert=new AlertDialog.Builder(Teacher.this).setTitle("Are Sure You Want to exit");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finishAffinity();
+                    }
+                });
+
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                   dialog.cancel();
+                    }
+                });
+                alert.show();
+            }
+        });
 
         attendClass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Teacher.this,StartClass.class));
+                startActivity(new Intent(Teacher.this,ClassEnter.class));
             }
         });
 
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_page_teacher,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.action_settings){
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/nileshteji/MarkUp")));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
